b0eb883facf3b49a1825fd21247859a96ed92a0c
suyeong2785 <qkdtpwls_@naver.com>
2023-12-29 23:27:34 +0900

    23/12/29 방수영 1.단어장앱 UI 구조잡기 완료

--- app/src/main/java/com/example/chapter7/AddActivity.kt
+++ app/src/main/java/com/example/chapter7/AddActivity.kt
@@ -2,13 +2,10 @@ package com.example.chapter7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_add)
     }
 }
